3d00fdbc099a8411709de87c1acfbf9d675a4a80
DominikColl <DAColl@students.fullsail.edu>
2020-09-08 15:56:23 -0400

    Form submission working client side

--- client/src/login.js
+++ client/src/login.js
@@ -8,19 +8,34 @@ import {
   Link
 } from "react-router-dom";
 class Login extends React.Component {
+    state={name:'',password:''}
     constructor(){
         super()
+    }
+    handleChange=event=>{
+        const {target}=event;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+const {name}=target
+this.setState({
+    [name]:value
+})
+    }
+    handleSubmit=async (event)=>{
+event.preventDefault()
+console.log(this.state)
+// const {name,password}=this.state
+// console.log(name+password)
     }
 render(){
     return(
         <div>
         <h2>Welcome to login page</h2>
   
-        <form>
+        <form onSubmit={this.handleSubmit}>
         <label>Name</label>
-        <input id='name' name='name' type='text'></input>
+        <input id='name' name='name' type='text' onChange={this.handleChange}></input>
         <label>Password</label>
-        <input id='password' name='password' type='text'></input>
+        <input id='password' name='password' type='text' onChange={this.handleChange}></input>
         <button type='submit'>Submit</button>
         </form>
       </div>
